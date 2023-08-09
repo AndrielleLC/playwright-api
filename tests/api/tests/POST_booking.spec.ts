0deb95b781182a6f9ad5118174bde21b21f061de
Andrielle Lígia Calegari <andrielle.calegari@ciandt.com>
2023-08-09 17:12:33 -0300

    ajustes nos teste e adiciona integracao com allure report

--- tests/api/tests/POST_booking.spec.ts
+++ tests/api/tests/POST_booking.spec.ts
@@ -1,11 +1,15 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
-import { postStaticRequest } from "../requests/POST_booking.request";
+import {
+  postDinamicRequest,
+  postStaticRequest,
+  randomFirstName,
+  randomLastName,
+} from "../requests/POST_booking.request";
 
 test.describe.parallel("API TESTING - PLAYWRIGHT", () => {
-  test("should be able to create a booking", async ({ request }) => {
+  test("STATIC - Should be able to create a booking", async ({ request }) => {
     const response = await postStaticRequest({ request });
-
     const responseJson = await response.json();
 
     expect(response.ok()).toBeTruthy();
@@ -14,5 +18,16 @@ test.describe.parallel("API TESTING - PLAYWRIGHT", () => {
     expect(responseJson.booking).toHaveProperty("lastname", "Brown");
     expect(responseJson.booking).toHaveProperty("totalprice", 111);
     expect(responseJson.booking).toHaveProperty("depositpaid", true);
+  });
+
+  test("DINAMIC - Should be able to create a booking", async ({ request }) => {
+    const response = await postDinamicRequest({ request });
+    const responseJson = await response.json();
+
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+
+    expect(responseJson.booking).toHaveProperty("firstname", randomFirstName);
+    expect(responseJson.booking).toHaveProperty("lastname", randomLastName);
   });
 });
