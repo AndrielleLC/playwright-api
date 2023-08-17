a2654ff3d0c5f03daddfdeb3801f1c646ab7e25a
Andrielle Lígia Calegari <andrielle.calegari@MacBook-Pro.local>
2023-08-17 13:59:12 -0300

    Some refactories

--- tests/api/tests/PUT_booking.spec.ts
+++ tests/api/tests/PUT_booking.spec.ts
@@ -6,9 +6,9 @@ import { postToken } from '../requests/POST_token.request';
 
 test.describe.parallel("API TESTING - PUT", () =>{
     test('Should update additional need from booking details', async ({ request }) => {
-        // const responseToken = await postToken ({request});
-        // const responseTokenJson = responseToken;
-        // console.log("New Token is: " + responseTokenJson);
+        const responseToken = await postToken ({request});
+        const responseTokenJson = responseToken;
+        console.log("New Token is: " + responseTokenJson);
 
         // PUT
         const response = await putRequest ({request});
