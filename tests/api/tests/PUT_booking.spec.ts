a1004b8800675f9b037f146c22b59180ef4a115f
Andrielle Lígia Calegari <andrielle.calegari@MacBook-Pro.local>
2023-08-17 14:04:10 -0300

    Some refactories

--- tests/api/tests/PUT_booking.spec.ts
+++ tests/api/tests/PUT_booking.spec.ts
@@ -5,22 +5,23 @@ import { postToken } from '../requests/POST_token.request';
 
 
 test.describe.parallel("API TESTING - PUT", () =>{
-    test('Should update additional need from booking details', async ({ request }) => {
+    test('should be able to update the booking details', async ({ request }) => {
         const responseToken = await postToken ({request});
         const responseTokenJson = responseToken;
         console.log("New Token is: " + responseTokenJson);
 
         // PUT
         const response = await putRequest ({request});
-        const putResponseBody = await response.json()
-        
+        const responseJson = await response.json();
+        console.log(responseJson);
         expect(response.ok()).toBeTruthy();
         expect(response.status()).toBe(200);
+
+        const putResponseBody = await response.json()
         expect(putResponseBody).toHaveProperty("firstname", "Jim");
         expect(putResponseBody).toHaveProperty("lastname", "Brown");
         expect(putResponseBody).toHaveProperty("totalprice", 111);
         expect(putResponseBody).toHaveProperty("depositpaid", true);
-        expect(putResponseBody.additionalneeds).not.toEqual("Breakfast");
     })
 })
 
